684cc34ec6699d9ccabf6be32dde8cd2c05f9b32
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-05 13:30:09 +0200

    Add GameMode enum and update main

--- UserInput/AnswerOption.py
+++ UserInput/AnswerOption.py
@@ -1,5 +1,8 @@
+from enum import Enum
+
+
 class AnswerOption:
-    def __init__(self, matching_inputs: list[str], return_value):
+    def __init__(self, matching_inputs: list[str], return_value: Enum):
         # if user input is one of strings in matching_inputs
         # then option return_value will be returned
         self.matching_inputs = matching_inputs
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+from Enums.GameMode import GameMode
 from UserInput.UserInputHandling import get_decision
 from UserInput.AnswerOption import AnswerOption
 
 if __name__ == '__main__':
 
     extended_version = get_decision("Do you want to play extended version?[y/n]: ",
-                                    [AnswerOption(['y', 'Y'], True),
-                                     AnswerOption(['n', 'N'], False)])
+                                    [AnswerOption(['y', 'Y'], GameMode.RPSLS),
+                                     AnswerOption(['n', 'N'], GameMode.RPS)])
 
     print(extended_version)
