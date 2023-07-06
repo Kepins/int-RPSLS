f499feb328e2ab88b092362ba242fc99f87d7bb0
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-06 10:26:51 +0200

    Add two gamemodes

--- GameController/Controller.py
+++ GameController/Controller.py
@@ -1,32 +1,27 @@
-from Enums.FiguresEnum import FiguresEnum
 from Enums.GameModeEnum import GameModeEnum
 from Enums.GameRestartEnum import GameRestartEnum
-from Figures.Lizard import Lizard
-from Figures.Paper import Paper
-from Figures.Rock import Rock
-from Figures.Scissors import Scissors
-from Figures.Spock import Spock
-from GameController.Game import Game
-from UserInput.AnswerOption import AnswerOption
-from UserInput.UserInputHandling import get_decision
+from GameController.GameRPS import GameRPS
+from GameController.GameRPSLS import GameRPSLS
+from UserInput.ExtendedVersionHandler import ExtendedVersionHandler
+from UserInput.RestartGameHandler import RestartGameHandler
 
 
 class Controller:
-    def start(self):
+    extendedVersionHandler = ExtendedVersionHandler()
+    restartGameHandler = RestartGameHandler()
+
+    def start(self) -> None:
 
         # player wants to play until is_playing is set to GameRestart.END_PROGRAM
         is_playing = GameRestartEnum.PLAY
         while is_playing != GameRestartEnum.END_PROGRAM:
-            game_mode = get_decision("Do you want to play extended version?[y/n]: ",
-                                     [AnswerOption(['y', 'Y'], GameModeEnum.RPSLS),
-                                      AnswerOption(['n', 'N'], GameModeEnum.RPS)])
+            game_mode: GameModeEnum = self.extendedVersionHandler.get_decision()
 
             game = None
             if game_mode == GameModeEnum.RPS:
-                game = Game([Rock(), Paper(), Scissors()])
+                game = GameRPS()
             elif game_mode == GameModeEnum.RPSLS:
-                game = Game([Rock(), Paper(), Scissors(), Lizard(), Spock()])
+                game = GameRPSLS()
 
-            is_playing = get_decision("Do you want to play again?[y/n]: ",
-                                      [AnswerOption(['y', 'Y'], GameRestartEnum.PLAY),
-                                       AnswerOption(['n', 'N'], GameRestartEnum.END_PROGRAM)])
+
+            is_playing = self.restartGameHandler.get_decision()
--- GameController/Game.py
+++ GameController/Game.py
@@ -1,6 +1,20 @@
+from Enums.GameResultEnum import GameResultEnum
 from Figures.Figure import Figure
+import random
 
 
 class Game:
     def __init__(self, figures: list[Figure]):
         self.figures = figures
+
+    def get_possible_choices(self) -> list[Figure]:
+        return self.figures
+
+    def set_user_choice(self, figure: Figure) -> None:
+        self.user_choice = figure
+
+    def set_opponent_choice(self) -> None:
+        self.opponent_choice = random.choice(self.figures)
+
+    def get_result(self) -> GameResultEnum:
+        return self.user_choice.beats(self.opponent_choice)
