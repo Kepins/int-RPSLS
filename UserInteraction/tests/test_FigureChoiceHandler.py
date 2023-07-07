c2e7a60e59e8ba05896c35c838c4cb3301fb963c
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-07 08:06:41 +0200

    Test wrong input FigureChoiceHandler

--- UserInteraction/tests/test_FigureChoiceHandler.py
+++ UserInteraction/tests/test_FigureChoiceHandler.py
@@ -27,3 +27,15 @@ def test_wants_last_figure():
 
     with mock.patch.object(builtins, 'input', lambda _: f'{len(figures)}'):
         assert handler.get_decision() == figures[-1]
+
+
+def test_bad_input_then_wants_last_figure():
+    # figures used in test case
+    figures = [Rock(), Paper(), Scissors(), Lizard(), Spock()]
+
+    handler = FigureChoiceHandler(figures)
+
+    input_iter = iter(['bad input', f'{len(figures)}'])
+
+    with mock.patch.object(builtins, 'input', lambda _: next(input_iter)):
+        assert handler.get_decision() == figures[-1]
